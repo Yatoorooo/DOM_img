20372195e48a578adf9585b5e34b4b95d68f3b8f
Yatoorooo <Egvarakcin@gmail.com>
2024-03-06 11:51:12 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg"];
+
+
+let randomIndex = Math.floor(Math.random() * images.length);
+
+
+let randomImagePath = "images/" + images[randomIndex];
+
+
+let imageElement = document.getElementById('randomImage');
+
+
+imageElement.src = randomImagePath;
